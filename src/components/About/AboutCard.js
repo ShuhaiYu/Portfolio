138276ae3739a1ad70c8a10adfaad0556d5c5d14
ShuhaiYu <1141071640@qq.com>
2023-11-14 21:01:22 +1100

    about done

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImPointRight } from "react-icons/im";
+import { GiAncientSword } from "react-icons/gi";
+import { IoBookSharp } from "react-icons/io5";
+import { MdOutlineCardTravel } from "react-icons/md";
 
 function AboutCard() {
   return (
@@ -8,33 +10,32 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br /> I am a final year student pursuing an Integrated MSc (IMSc)
-            in Maths and Computing at BIT Mesra.
+            Hello, I'm <span className="purple">Shuhai Yu</span> from Melbourne, VIC, Australia.
+            <br /> I'm currently pursuing a Master of Information Technology at Monash University.
             <br />
-            Additionally, I am currently employed as a software developer at
-            Juspay.
+            In addition, I obtained my Bachelor of Civil Engineering (Hons) from RMIT University and studied Project Management and Engineering Contracts at Tongji University in Shanghai.
+            <br />
+            I am passionate about software development and I am always eager to learn new technologies.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            In my leisure, I enjoy:
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <GiAncientSword /> Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <IoBookSharp /> Reading books
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <MdOutlineCardTravel /> Travelling
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+          <p style={{ color: "#6db1ff" }}>
+            "Striving to build innovative solutions that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Shuhai Yu</footer>
         </blockquote>
       </Card.Body>
     </Card>
